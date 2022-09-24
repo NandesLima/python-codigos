beace77055bb17ca335938397b3c32ab07caede0
Ernandes Lima <euernandeslima@gmail.com>
2022-09-23 23:36:22 -0300

    Atualização de modulos/02 - OS

--- codigos/modulos/02-OS.py
+++ codigos/modulos/02-OS.py
@@ -1,7 +1,19 @@
-from msilib import PID_TITLE
 import os # Acessa objetos e funções que mexem no sistema operacional
 
 print(os.path.exists('teste.txt')) # Verifica se existe um determinado arquivo
-os.system('pause') # Pausa o programa até ter uma tecla ser precionada e o programa continua
-os.system('cls') # Limpa a tela do terminal
-# O método .system() acessa o sistema da máquina
+
+
+limpar = 'cls' # Comando de limpar a tela do windows (shell)
+limpar = 'clear' # Comando de limpar a tela do linux (unix/bash)
+
+pausar = 'pause' # Pausa a execução no windows (shell)
+pausar = 'read -p "Precione ENTER para continuar..."' # Pausa a execução no linux (unix/bash)
+# read métodode de leitura do bash, aguarda precionar ENTER para continuar
+# -s modo silencioso, aguarda qualquer tecla ser precionada, mas não guarda o seu valor
+# -r coloca no modo rudimentar necessário para que considere teclas como /
+# -p especifica o prompt
+
+os.system(pausar) # Pausa o programa até ter uma tecla ser precionada e o programa continua
+os.system(limpar) # Limpa a tela do terminal no Linux
+
+# O método .system() utiliza comandos do terminal
